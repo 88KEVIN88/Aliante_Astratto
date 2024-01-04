119490f629254bdf8da6b1d381675e8cf7e9f335
azzol <azzol@PC001>
2024-01-04 14:05:00 +0100

    struttura base classe aliante

--- Aliante_Astratto/Classi/Aliante.cs
+++ Aliante_Astratto/Classi/Aliante.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Aliante_Astratto
 {
-    internal class Aliante
+    internal class Aliante : Composite
     {
+        private List<Composite> _compotenti;
+
+        public List<Composite> Componenti
+        {
+            get { return _compotenti; }
+            set { _compotenti = value; }
+        }
+        public Aliante()
+        {
+            Componenti = new List<Composite>();
+        }
+        public Aliante(List<Composite> compotenti, List<Composite> componenti)
+        {
+            Componenti = componenti;       
+        }
+        public Aliante(Aliante vecchio)
+        {
+            Componenti = vecchio.Componenti;
+        }
     }
 }
