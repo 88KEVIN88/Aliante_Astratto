10ebe7b5fdba18c33a61c8d2d84f955715c9d89a
azzol <azzol@PC001>
2024-01-04 14:17:17 +0100

    metodi della classe aliante

--- Aliante_Astratto/Classi/Aliante.cs
+++ Aliante_Astratto/Classi/Aliante.cs
@@ -27,5 +27,50 @@ namespace Aliante_Astratto
         {
             Componenti = vecchio.Componenti;
         }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Aliante otherAliante) || otherAliante.Componenti.Count != Componenti.Count)
+            {
+                return false;
+            }
+
+            return Componenti.SequenceEqual(otherAliante.Componenti);
+
+        }
+        public override void Remove(int index)
+        {
+            Componenti.RemoveAt(index);
+        }
+        public override Composite Get(int index)
+        {
+            if(Componenti.Count > index)
+            {
+                return Componenti[index];
+            }
+            return null;
+        }
+        public override string ToString()
+        {
+            string stringa = "";
+            foreach(var componetni in Componenti)
+            {
+                stringa += componetni.ToString();
+            }
+            return stringa;
+        }
+        public override double Price()
+        {
+            double totale = 0;
+            foreach(var componetni in Componenti)
+            {
+                totale += componetni.Price();
+
+            }
+            return totale;
+        }
+        public override void Add(Composite componente)
+        {
+            Componenti.Add(componente);
+        }
     }
 }
