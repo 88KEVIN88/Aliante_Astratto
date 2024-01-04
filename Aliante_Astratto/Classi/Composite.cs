0ab96191637b3efeb7bec6f515ac27f498ba2d5c
azzol <azzol@PC001>
2024-01-04 14:01:55 +0100

    Classe astratta

---
+++ Aliante_Astratto/Classi/Composite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliante_Astratto
+{
+    public abstract class Composite
+    {
+        public abstract void Add(Composite component);
+        public abstract void Remove(int index);
+        public abstract Composite Get(int index);
+        public new abstract bool Equals(object obj);
+        public new abstract string ToString();
+        public abstract double Price();
+
+
+    }
+}
