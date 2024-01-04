ab2e4e30f0f7300b5603bdc0eada0d86ab6deef6
azzol <azzol@PC001>
2024-01-04 14:21:54 +0100

    Controlli input classe Ala

--- Aliante_Astratto/Classi/Ala.cs
+++ Aliante_Astratto/Classi/Ala.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aliante_Astratto
 {
-    internal class Ala
+    internal class Ala : Composite
     {
+        private double _length;
+        private double _width;
+
+        private double Lenght
+        {
+            get { return _length; }
+            set
+            {
+                if (_length > 0)
+                {
+                    _length = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La lunghezza deve essere >0");
+                }
+            }
+        }
+
+        public double Width
+        {
+            get { return _width; }
+            set
+            {
+                if(_width > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La larghezza alare deve essere >0");
+                }
+            }
+        }
+        public Ala()
+        {
+            _width=0;
+            _length=0;
+        }
+
     }
 }
