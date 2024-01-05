59baa556cec9df7bd6c6fe785e6b825651c2d9f7
azzol <azzol@PC001>
2024-01-05 15:44:16 +0100

    Metodi classe Coda

--- Aliante_Astratto/Classi/Coda.cs
+++ Aliante_Astratto/Classi/Coda.cs
@@ -8,6 +8,63 @@ namespace Aliante_Astratto
 {
     internal class Coda : Composite
     {
-       
+        public double _lenght;
+
+        public double Lenght
+        {
+            get { return _lenght; }
+            set
+            {
+                if(_lenght >0)
+                {
+                    _lenght = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La Lunghezza deve essere > 0");
+                }
+            }
+        }
+        public Coda()
+        {
+           Lenght = 0;
+
+        }
+        public Coda(double lenght)
+        {
+            Lenght= lenght;
+        }
+        public Coda(Coda vecchia)
+        {
+            Lenght = vecchia.Lenght;
+        }
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Coda other))
+            {
+                return false;
+            }
+            return Lenght == other.Lenght;
+        }
+        public override void Add(Composite component)
+        {
+           
+        }
+        public override void Remove(int index)
+        {
+        
+        }
+        public override Composite Get(int index)
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+            return $"Lunghezza della coda:{Lenght}";
+        }
+        public override double Price()
+        {
+            return Lenght * 3;
+        }
     }
 }
