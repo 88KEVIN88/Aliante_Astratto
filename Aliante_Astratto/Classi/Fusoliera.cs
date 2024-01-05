748f37b9001bbb574603a3f22706dd3679102b18
azzol <azzol@PC001>
2024-01-05 15:51:40 +0100

    Metodi della classe fusoliera

--- Aliante_Astratto/Classi/Fusoliera.cs
+++ Aliante_Astratto/Classi/Fusoliera.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Aliante_Astratto
 {
-    internal class Fusoliera
+    internal class Fusoliera : Composite
     {
+        private double _lenght;
+        private string _materials;
+        public double Lenght
+        {
+            get { return _lenght; }
+            set
+            {
+                if(_lenght>0)
+                {
+                    _lenght = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La lunghezza della fusoliera deve essere > 0");
+
+                }
+            }
+        }
+        public string Materials
+        {
+            get { return _materials; }
+            set { _materials = value; }
+        }
+        public Fusoliera()
+        {
+            Lenght = 0;
+            Materials = string.Empty;
+        }
+        public Fusoliera(double lenght,string materiale)
+        {
+            Lenght=lenght;
+            Materials = materiale;
+        }
+        public override bool Equals(object obj)
+        {
+
+            if (!(obj is Coda other))
+            {
+                return false;
+            }
+            return Lenght == other.Lenght;
+        }
+        public override void Add(Composite component)
+        {
+           
+        }
+        public override void Remove(int index)
+        {
+
+        }
+        public override Composite Get(int index)
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+           return $"Lunghezza:{Lenght};Materiale della fusolieras:{Materials}";   
+        }
     }
+
 }
